be7e8a8c06ff611072d7582cd415ac382979798c
skava <sarath_kavati@pa.com.au>
2015-10-07 13:29:13 +1100

    test

--- iPOS/src/appium/MobileTest.java
+++ iPOS/src/appium/MobileTest.java
@@ -1,6 +1,42 @@
 package appium;
 
+
+import java.net.MalformedURLException;
+import java.net.URL;
+ 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+ 
 public class MobileTest {
 	
-
+	WebDriver driver;
+	
+	@BeforeClass
+	public void setUp() throws MalformedURLException{
+		DesiredCapabilities capabilities = new DesiredCapabilities();
+		capabilities.setCapability("device", "Android");
+		capabilities.setCapability(CapabilityType.BROWSER_NAME, ""); //Name of mobile web browser to automate. Should be an empty string if automating an app instead.
+		capabilities.setCapability(CapabilityType.VERSION, "4.1.2");
+		capabilities.setCapability(CapabilityType.PLATFORM, "Windows");
+		capabilities.setCapability("app-package", "APP PACKAGE"); //Replace with your app's package
+		capabilities.setCapability("app-activity", "APP PACKAGE.ANDROID ACTIVITY"); //Replace with app's Activity
+		driver = new RemoteWebDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+	}
+	
+	@Test
+	public void Cal(){
+		driver.findElement(By.name("Weather")).click();
+	}
+	
+	@AfterClass
+	public void tearDown(){
+		driver.quit();
+	}
+ 
 }
