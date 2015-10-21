f7abe880c9129fea9a2ad212df40e8621cd905fb
skava <sarath_kavati@pa.com.au>
2015-10-21 12:28:46 +1100

    created screen cast package + HTML customized test report

--- iPOS/src/base/BaseMethods.java
+++ iPOS/src/base/BaseMethods.java
@@ -1,6 +1,5 @@
 package base;
 
-
 import java.awt.Rectangle;
 //import java.awt.Point;
 //import java.awt.Rectangle;
@@ -28,322 +27,298 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-
-
+import com.relevantcodes.extentreports.ExtentReports;
 
 public class BaseMethods {
-public static  WebDriver driver ;
+	public static WebDriver driver;
 
-	
-//public  BaseMethods(WebDriver driver)
-//{
-//
-//	BaseMethods.driver = driver;
-//
-//	}
-	
-		
-	
+	// public BaseMethods(WebDriver driver)
+	// {
+	//
+	// BaseMethods.driver = driver;
+	//
+	// }
 
-	
-	
-	
-	
+	public static WebDriver GetDriver(String BrowserName) {
 
-  
-	 public static WebDriver GetDriver(String BrowserName){
-   
-       try{                  
-           
-           switch (BrowserName.toLowerCase())
-           {
-               case "ie":    
-//              	 File file = new File("C:/Selenium/IE/IEDriverServer.exe");
-//              	 System.setProperty("webdriver.ie.driver", file.getAbsolutePath());
-              	 System.setProperty("webdriver.ie.driver", "C:/Selenium/IE/IEDriverServer.exe");
-                   driver = new InternetExplorerDriver();                     
-                   break;
+		try {
 
-               case "chrome":
-                   driver = new ChromeDriver();
-                   break;
+			switch (BrowserName.toLowerCase()) {
+			case "ie":
+				// File file = new File("C:/Selenium/IE/IEDriverServer.exe");
+				// System.setProperty("webdriver.ie.driver",
+				// file.getAbsolutePath());
+				System.setProperty("webdriver.ie.driver", "C:/Selenium/IE/IEDriverServer.exe");
+				driver = new InternetExplorerDriver();
+				break;
 
-               case "firefox":
-                   driver = new FirefoxDriver();
-                   break;
+			case "chrome":
+				driver = new ChromeDriver();
+				break;
 
+			case "firefox":
+				driver = new FirefoxDriver();
+				break;
 
-//               default:
-//              	 System.out.println("Sorry, invalid selection of ''Browser name''.");                     
-//                   break;
-           }
-           
-           
-//           BaseMethods.driver.manage().window().maximize();
-          // BaseMethods.driver.get(url);// also works
-//           BaseMethods.driver.navigate().to(url);
-           
-           
-       }         catch (NoSuchElementException e){         
+			// default:
+			// System.out.println("Sorry, invalid selection of ''Browser
+			// name''.");
+			// break;
+			}
 
-      	 System.out.println(e.getMessage());
+			// BaseMethods.driver.manage().window().maximize();
+			// BaseMethods.driver.get(url);// also works
+			// BaseMethods.driver.navigate().to(url);
 
-       }
-       return driver ;
-       
-	 }
+		} catch (NoSuchElementException e) {
 
-	 public static void GetDriver(){
-		    
-	       GetDriver("IE"); // Using regular method overloads>>> as parameter url has dynamic value "GlobalVariables.iPOSServer"
-	    }
-	 
-	 
-	 public static void SwitchFrame(WebDriver driver,String FrameValue,String FrameType,int WaitTimeInSec)
-   {
-       try
-       {
-           driver.switchTo().defaultContent();
-           switch (FrameType.toLowerCase())
-           {
-               case "id":
-                   WaitForElement(driver,By.id(FrameValue), WaitTimeInSec);
-                   driver.switchTo().frame(FrameValue);
-                   break;
+			System.out.println(e.getMessage());
 
-               case "default":
-                   driver.switchTo().defaultContent();                     
-                   break;
+		}
+		return driver;
 
-               case "name":
-                   WaitForElement(driver,By.name(FrameValue), WaitTimeInSec);
-                   driver.switchTo().frame(FrameValue);
-                   break;
-               
-
-               default:
-              	 System.out.println("Sorry, invalid selection of ''ByType''.");
-                   break;
-           }
-
-           
-       }
-       catch (NoSuchElementException e){         
-
-      	 System.out.println(e.getMessage());
-       }
-   }
-	
-	 public static void SwitchFrame(WebDriver driver,String FrameValue){
-		SwitchFrame(driver,FrameValue,"ID",30);
-	 }
-	 
-	 
-	 
-	 public static void SwitchWindow(WebDriver driver,String Title, int WindowCount, int WaitTimeInSec) throws InterruptedException  
-   {
-       try
-       {
-           // ..... METHOD 1 .......
-           String BaseWindow = driver.getWindowHandle();
-
-          
-           int count = 0;
-           while ((driver.getWindowHandles().size()) != WindowCount)
-           {
-               count++;                 
-               Thread.sleep(1000);
-              
-               if (count == 30)
-               {
-              	 System.out.println("Window with title '" + Title + "' still not found even after  '" + WaitTimeInSec + "' seconds");
-                   break;
-               }
-
-               if (driver.getWindowHandles().size() == WindowCount) break;
-           }
-
-           Set<String> handles = driver.getWindowHandles();
-
-           for (String handle:handles)
-              
-           {
-               
-               if (handle != BaseWindow)
-               {
-
-                   if (driver.switchTo().window(handle).getTitle().contains(Title))
-                   {
-                  	 System.out.println("Window "+ WindowCount +" title: "+driver.getTitle());
-                       break;
-
-                   }                      
-
-                 
-               }
-           }
-           
-       }
-   
-       catch (NoSuchElementException e){  
-
-
-       }
-   }
-	 
-	 public static void SwitchWindow(WebDriver driver,String Title, int WindowCount) throws InterruptedException
-   {
-		 SwitchWindow(driver,Title, WindowCount,10) ;
-   }
-
-
-	 public static void WaitForElement( WebDriver driver,By by, int timeoutInSeconds)
-   {
-       
-       //WebDriverWait wait =  new WebDriverWait(driver, Timeout.seconds(timeoutInSeconds);
-       WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
-
-       //wait.Until(drv => drv.FindElement(by)); // meothod 1
-       wait.until(ExpectedConditions.visibilityOfElementLocated(by)); // method 2 works for visible element only
-
-
-   }
-	 
-	 public static void WaitForElement( WebDriver driver,By by){ // over load function to have default timeoutinSeconds=10
-		 WaitForElement(driver,by,10);
-	 }
-	 
-	 
-	 public static <IWebElement> WebElement  GetElement(WebDriver driver,By by, int timeoutInSeconds)
-   {
-      
-       if (timeoutInSeconds > 0)
-       {
-      	 WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
-           //return wait.Until(drv => drv.FindElement(by));
-      	 wait.until(ExpectedConditions.visibilityOfElementLocated(by));
-       }
-       return driver.findElement(by);
-   }
-	 
-	 public static <IWebElement> WebElement  GetElement(WebDriver driver,By by){
-		 
-		 return GetElement(driver,by,10);
-	 }
-	 
-	 
-	 
-	 public static  List <WebElement> GetElements(WebDriver driver,By by, int timeoutInSeconds)
-   {
-       
-       if (timeoutInSeconds > 0)
-       {
-      	 
-           WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);     
-           wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(by));
-       }
-       return driver.findElements(by);
-   }
-	 
-	 // to click on web element
-	 public static void ClickElement(WebElement WebElement)
-   {
-       try
-       {
-           WebElement.click();
-
-       }
-       catch (NoSuchElementException e)
-       {
-      	 System.out.println(e.getMessage());
-
-       }
-
-
-   }
-	 
-	 
-	 // To double click on Web Element 
-   public static void DoubleClickElement(WebElement WebElement){
-   
-       try{
-       
-           //create Actions object
-      	 Actions act = new Actions(driver);
-           //Use DoubleClick method to double click on any element
-           act.doubleClick(WebElement).build().perform();
-          
-           
-       }
-       catch (NoSuchElementException e){
-       
-      	 System.out.println(e.getMessage());
-           
-       }
-     
-
-   }
-   
-   // To  keyin on Web Element 
-   public static void KeyInElement(WebElement WebElement, String Value)
-   {
-       try
-       {
-           WebElement.sendKeys(Value);
-
-       }
-       catch (NoSuchElementException e)
-       {
-      	 System.out.println("Unable to key in value " + Value + " on web element");
-
-       }
-   }
-	 
-   
-// to select an element from given list of selection box            
-   public static void SelectElement(WebElement WebElement, String Value)
-   {
-       try
-       {
-           Select select = new Select(WebElement);
-           select.selectByValue(Value);
-       }
-       catch (NoSuchElementException e)
-       {
-      	 System.out.println("Unable to select value " + Value);
-
-       }
-
-
-   }
-
-   // To take screen shot of page
-   public  static void captureScreenShot(String obj) throws IOException {
-       File screenshotFile=((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-       FileUtils.copyFile(screenshotFile,new File("Screenshots\\"+obj+""+GetTimeStampValue()+".png"));
- }
-
- public static  String GetTimeStampValue()throws IOException{
-
-         Calendar cal = Calendar.getInstance();       
-          java.util.Date time=cal.getTime();
-          String timestamp=time.toString();
-             System.out.println(timestamp);
-             String systime=timestamp.replace(":", "-");
-             System.out.println(systime);
-         return systime;
-
- }
-//taking screenshot of a specific element
- public void takeScreenshotElement(WebElement element) throws IOException {
-	    WrapsDriver wrapsDriver = (WrapsDriver) element;
-	    File screenshot = ((TakesScreenshot) wrapsDriver.getWrappedDriver()).getScreenshotAs(OutputType.FILE);
-	    Rectangle rectangle = new Rectangle(element.getSize().width, element.getSize().height);
-	    Point location = element.getLocation();
-	    BufferedImage bufferedImage = ImageIO.read(screenshot);
-	    BufferedImage destImage = bufferedImage.getSubimage(location.x, location.y, rectangle.width, rectangle.height);
-	    ImageIO.write(destImage, "png", screenshot);
-	    File file = new File("//path//to");
-	    FileUtils.copyFile(screenshot, file);
 	}
+
+	public static void GetDriver() {
+
+		GetDriver("IE"); // Using regular method overloads>>> as parameter url
+							// has dynamic value "GlobalVariables.iPOSServer"
+	}
+
+	public static void SwitchFrame(WebDriver driver, String FrameValue, String FrameType, int WaitTimeInSec) {
+		try {
+			driver.switchTo().defaultContent();
+			switch (FrameType.toLowerCase()) {
+			case "id":
+				WaitForElement(driver, By.id(FrameValue), WaitTimeInSec);
+				driver.switchTo().frame(FrameValue);
+				break;
+
+			case "default":
+				driver.switchTo().defaultContent();
+				break;
+
+			case "name":
+				WaitForElement(driver, By.name(FrameValue), WaitTimeInSec);
+				driver.switchTo().frame(FrameValue);
+				break;
+
+			default:
+				System.out.println("Sorry, invalid selection of ''ByType''.");
+				break;
+			}
+
+		} catch (NoSuchElementException e) {
+
+			System.out.println(e.getMessage());
+		}
+	}
+
+	public static void SwitchFrame(WebDriver driver, String FrameValue) {
+		SwitchFrame(driver, FrameValue, "ID", 30);
+	}
+
+	public static void SwitchWindow(WebDriver driver, String Title, int WindowCount, int WaitTimeInSec)
+			throws InterruptedException {
+		try {
+			// ..... METHOD 1 .......
+			String BaseWindow = driver.getWindowHandle();
+
+			int count = 0;
+			while ((driver.getWindowHandles().size()) != WindowCount) {
+				count++;
+				Thread.sleep(1000);
+
+				if (count == 30) {
+					System.out.println("Window with title '" + Title + "' still not found even after  '" + WaitTimeInSec
+							+ "' seconds");
+					break;
+				}
+
+				if (driver.getWindowHandles().size() == WindowCount)
+					break;
+			}
+
+			Set<String> handles = driver.getWindowHandles();
+
+			for (String handle : handles)
+
+			{
+
+				if (handle != BaseWindow) {
+
+					if (driver.switchTo().window(handle).getTitle().contains(Title)) {
+						System.out.println("Window " + WindowCount + " title: " + driver.getTitle());
+						break;
+
+					}
+
+				}
+			}
+
+		}
+
+		catch (NoSuchElementException e) {
+
+		}
+	}
+
+	public static void SwitchWindow(WebDriver driver, String Title, int WindowCount) throws InterruptedException {
+		SwitchWindow(driver, Title, WindowCount, 10);
+	}
+
+	public static void WaitForElement(WebDriver driver, By by, int timeoutInSeconds) {
+
+		// WebDriverWait wait = new WebDriverWait(driver,
+		// Timeout.seconds(timeoutInSeconds);
+		WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
+
+		// wait.Until(drv => drv.FindElement(by)); // meothod 1
+		wait.until(ExpectedConditions.visibilityOfElementLocated(by)); // method
+																		// 2
+																		// works
+																		// for
+																		// visible
+																		// element
+																		// only
+
+	}
+
+	public static void WaitForElement(WebDriver driver, By by) { // over load
+																	// function
+																	// to have
+																	// default
+																	// timeoutinSeconds=10
+		WaitForElement(driver, by, 10);
+	}
+
+	public static <IWebElement> WebElement GetElement(WebDriver driver, By by, int timeoutInSeconds) {
+
+		if (timeoutInSeconds > 0) {
+			WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
+			// return wait.Until(drv => drv.FindElement(by));
+			wait.until(ExpectedConditions.visibilityOfElementLocated(by));
+		}
+		return driver.findElement(by);
+	}
+
+	public static <IWebElement> WebElement GetElement(WebDriver driver, By by) {
+
+		return GetElement(driver, by, 10);
+	}
+
+	public static List<WebElement> GetElements(WebDriver driver, By by, int timeoutInSeconds) {
+
+		if (timeoutInSeconds > 0) {
+
+			WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
+			wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(by));
+		}
+		return driver.findElements(by);
+	}
+
+	// to click on web element
+	public static void ClickElement(WebElement WebElement) {
+		try {
+			WebElement.click();
+
+		} catch (NoSuchElementException e) {
+			System.out.println(e.getMessage());
+
+		}
+
+	}
+
+	// To double click on Web Element
+	public static void DoubleClickElement(WebElement WebElement) {
+
+		try {
+
+			// create Actions object
+			Actions act = new Actions(driver);
+			// Use DoubleClick method to double click on any element
+			act.doubleClick(WebElement).build().perform();
+
+		} catch (NoSuchElementException e) {
+
+			System.out.println(e.getMessage());
+
+		}
+
+	}
+
+	// To keyin on Web Element
+	public static void KeyInElement(WebElement WebElement, String Value) {
+		try {
+			WebElement.sendKeys(Value);
+
+		} catch (NoSuchElementException e) {
+			System.out.println("Unable to key in value " + Value + " on web element");
+
+		}
+	}
+
+	// to select an element from given list of selection box
+	public static void SelectElement(WebElement WebElement, String Value) {
+		try {
+			Select select = new Select(WebElement);
+			select.selectByValue(Value);
+		} catch (NoSuchElementException e) {
+			System.out.println("Unable to select value " + Value);
+
+		}
+
+	}
+
+	// To take screen shot of page
+	public static void captureScreenShot(String obj) throws IOException {
+		File screenshotFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(screenshotFile, new File("Screenshots\\" + obj + "" + GetTimeStampValue() + ".png"));
+	}
+
+	public static String GetTimeStampValue() throws IOException {
+
+		Calendar cal = Calendar.getInstance();
+		java.util.Date time = cal.getTime();
+		String timestamp = time.toString();
+		System.out.println(timestamp);
+		String systime = timestamp.replace(":", "-");
+		System.out.println(systime);
+		return systime;
+
+	}
+
+	// taking screenshot of a specific element
+	public void takeScreenshotElement(WebElement element) throws IOException {
+		WrapsDriver wrapsDriver = (WrapsDriver) element;
+		File screenshot = ((TakesScreenshot) wrapsDriver.getWrappedDriver()).getScreenshotAs(OutputType.FILE);
+		Rectangle rectangle = new Rectangle(element.getSize().width, element.getSize().height);
+		Point location = element.getLocation();
+		BufferedImage bufferedImage = ImageIO.read(screenshot);
+		BufferedImage destImage = bufferedImage.getSubimage(location.x, location.y, rectangle.width, rectangle.height);
+		ImageIO.write(destImage, "png", screenshot);
+		File file = new File("//path//to");
+		FileUtils.copyFile(screenshot, file);
+	}
+	
+	// To generate test results report in html format
+	
+	private static ExtentReports extent ;
+	 public static ExtentReports getInstance() {
+	        if (extent == null) {
+	            extent = new ExtentReports("\\\\sydfps10\\cst\\QA\\Sarath Back up\\TestingHelper\\TestResultsReport.html", true) ; //\\sydfps10\cst\QA\Sarath Back up\TestingHelper\TestResultsReport.html
+	            
+	            // optional
+	            extent.config()
+	                .documentTitle("Automation Report")
+	                .reportName("Regression")
+	                .reportHeadline("");
+	               
+	            // optional
+	            extent
+	                .addSystemInfo("Selenium Version", "2.47")
+	                .addSystemInfo("Environment", "QA") ;
+	        }
+	        return extent ;
+	    }
 }
--- iPOS/src/tests/TestCases.java
+++ iPOS/src/tests/TestCases.java
@@ -1,25 +1,46 @@
 package tests;
 
 import org.testng.annotations.Test;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.asserts.Assertion;
 import org.testng.asserts.SoftAssert;
-import org.testng.annotations.BeforeMethod; 
+
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
+import com.relevantcodes.extentreports.common.ExtentManager;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+
 import base.BaseMethods;
 import base.GlobalVariables;
 import pageObjects.LoginPage;
+
 
 public class TestCases {
 	public static WebDriver driver;
 	
 	public  Assertion hardAssert = new Assertion(); 
 	public  SoftAssert softAssert = new SoftAssert(); 
-
+	private ExtentReports extent = BaseMethods.getInstance();
+	 
+//	@BeforeClass
+//	public void beforefclass() throws Exception {		
+//	
+//	}
 	@BeforeMethod
-	public void beforeMethod() throws Exception {
-		 
+	public void beforeMethod() throws Exception {		
 		driver= BaseMethods.GetDriver("chrome");  
 		driver.manage().window().maximize();        
        BaseMethods.driver.navigate().to("http://" + GlobalVariables.iPOSServer + "/iPOS/Web/Login.aspx"); 
@@ -28,18 +49,41 @@ public class TestCases {
 	@AfterMethod
 	public void AfterMethod() throws Exception {
 		 
-		//driver.quit();
+		driver.quit();		
+      
+	}
+	
+	@AfterClass
+	public void AfterClass() throws Exception {	 
+	
+       extent.flush();    
+       //  terminates extent and clears the underlying stream
+        extent.close();
 	}
 	
 	@Test
 
- public  void test1() throws InterruptedException { //throws InterruptedException {
-				
-	 LoginPage page = PageFactory.initElements(driver, LoginPage.class);	
-     BaseMethods.KeyInElement(page.UserNameText, "rh");            
-     BaseMethods.KeyInElement(page.PasswordText, "rh1");           
+ public  void test1() throws InterruptedException, IOException { //throws InterruptedException {
+	 ExtentTest test = extent.startTest("Test 1", "Login by RH");			
+	 LoginPage page = PageFactory.initElements(driver, LoginPage.class);
+	 // enter user name in text box
+	 test.log(LogStatus.INFO, "User name", "Entering user name in user name field");     
+     BaseMethods.KeyInElement(page.UserNameText, "rh");   
+     test.log(LogStatus.INFO, "Password", "Entering password in password field");
+     BaseMethods.KeyInElement(page.PasswordText, "rh1"); 
+     test.log(LogStatus.WARNING, "click on login button", "clicking  on login button");
      BaseMethods.ClickElement(page.LoginButton);
+     Thread.sleep(2000);
+     File screenshotFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+	 FileUtils.copyFile(screenshotFile, new File("C:\\Screenshots\\Test1_CP1.png"));     
+     Thread.sleep(2000);
+     test.log(LogStatus.INFO, "Snapshot below: "+ test.addScreenCapture("C:\\Screenshots\\Test1.png"));  
+     
      System.out.println("User name and password entered successfully");   
+//     test.log(LogStatus.ERROR, "testing error ", "error log");
+//     test.log(LogStatus.ERROR, "testing error ", "error log");
+     test.log(LogStatus.PASS, "StepName", "PASS Details");
+     extent.endTest(test);
 //     BaseMethods.SwitchFrame(driver,"topFrame");            
 //     BaseMethods.ClickElement(page.Create);           
 //     BaseMethods.SwitchFrame(driver,"mainFrame");
@@ -83,14 +127,22 @@ public class TestCases {
 	}	   
 	
 	@Test
-	 public void test2() throws InterruptedException { //throws InterruptedException {
-			
+	 public void test2() throws InterruptedException, IOException { //throws InterruptedException {		
+		ExtentTest test = extent.startTest("Test 2", "Login by SS");		
 		 LoginPage page = PageFactory.initElements(driver, LoginPage.class);	
 	     BaseMethods.KeyInElement(page.UserNameText, "ss");            
 	     BaseMethods.KeyInElement(page.PasswordText, "ss1");           
 	     BaseMethods.ClickElement(page.LoginButton);
-	     System.out.println("User name and password entered successfully");            
-//	     BaseMethods.SwitchFrame("mainFrame");
+	     System.out.println("User name and password entered successfully");    
+	     File screenshotFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		 FileUtils.copyFile(screenshotFile, new File("C:\\Screenshots\\Test2_CP1.png"));
+		 Thread.sleep(2000);
+		test.log(LogStatus.INFO, "Snapshot below: "+ test.addScreenCapture("C:\\Screenshots\\Test2_CP1.png"));   	     
+	     Thread.sleep(2000); 
+//	    extent.(LogStatus.PASS, "Snapshot below: " ,"./Test2_CP1.png","C:\\Screenshots\\Test2_CP1.png"); 
+	    
+	     extent.endTest(test);
+//	     BaseMethods.SwitchFrame("mainFrame");;
 //	     BaseMethods.SwitchFrame("topFrame");            
 //	     BaseMethods.ClickElement(page.Create);           
 //	     BaseMethods.SwitchFrame("mainFrame");
